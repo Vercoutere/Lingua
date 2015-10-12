787069f85df6baf3555adb13f22b9a5e14aa1022
Jonathan Vercoutere <jonathan@foci.be>
2015-10-12 19:08:03 +0200

    Some functional changes + added Travis CI

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 ## Lingua language library
 
+[![Build Status](https://travis-ci.org/Vercoutere/Lingua.svg)](https://travis-ci.org/Vercoutere/Lingua)
+
 Lingua is a simple library that links language codes to language names in both English and the corresponding language's native translation.
--- spec/Lingua/LanguageSpec.php
+++ spec/Lingua/LanguageSpec.php
@@ -12,33 +12,18 @@ class LanguageSpec extends ObjectBehavior
         $this->shouldHaveType('Lingua\Language');
     }
 
-    /**
-     * Find a language by it's code.
-     * @param   String    $code    ISO 639-1 code, for availlable codes check https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
-     * @returns Array    An array containing the language code, name and native name
-     */
     function it_returns_a_language_array_by_code() {
 
         $language = $this->getLanguageByCode('za');
         $this->checkLanguageArray($language);
     }
 
-    /**
-     * Find a language by it's name.
-     * @param   String   $name   The language name in English
-     * @returns Array    An array containing the language code, name and native name
-     */
     function it_returns_a_language_array_by_name() {
 
         $language = $this->getLanguageByName('Zhuang');
         $this->checkLanguageArray($language);
     }
 
-    /**
-     * Find a language by it's native name.
-     * @param   String    $name    The language name in the corresponding language
-     * @returns Array    An array containing the language code, name and native name
-     */
     function it_returns_a_language_array_by_native_name() {
 
         $language = $this->getLanguageByNativeName('Vaƅcueŋƅ');
